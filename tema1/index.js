3da5147c3f564cb296a5644aadb7039bb49a3a9f
Ernesto Campos Villa <holasoy@misterernest.com>
2019-06-20 21:09:26 -0500

    add introduction console log

---
+++ tema1/index.js
@@ -0,0 +1,30 @@
+// mostrar datos en consola
+console.log("Hola mundo JS!");
+
+// declarar variables globales
+var nombre;
+// asignar valor
+nombre = "Ernesto";
+
+// uso de la variable y concatenar
+console.log("hola " + nombre);
+
+// declarar variable e inicializar
+var apellido = "Campos";
+
+console.log("hola " + nombre + " " + apellido);
+
+//declarar dos variables en la misma linea
+var nombre1 = "Ernesto",
+  apellido1 = "Campos";
+
+console.log("hola " + nombre1 + " " + apellido1);
+
+// lenguaje debilmente tipado
+
+edad = 28;
+console.log("tengo " + edad + " años");
+// cambia facilmente de entero a string
+edad = "ventiocho";
+// y funciona igual
+console.log("tengo " + edad + " años");
