638fc4cefe13eb1716a4c44ef5a6438792251c8a
Ernesto Campos Villa <ecampos@aventi.com.co>
2019-06-25 17:40:45 -0500

    add first step with object

---
+++ tema6/index.js
@@ -0,0 +1,14 @@
+// objetos
+
+// Como seria con function
+
+// var primerNombre = 'Elias';
+// var segundoNombre = 'Ernesto';
+
+// function imprimirNombreEnMayuscula(nombre) {
+//   nombre = nombre.toUpperCase()
+//   console.log(nombre); 
+// }
+
+// imprimirNombreEnMayuscula(primerNombre);
+// imprimirNombreEnMayuscula(segundoNombre);
