cc5ad22d3d1b0262a97202bc8cdafd2bffc0e471
Ernesto Campos Villa <holasoy@misterernest.com>
2019-07-13 23:36:50 -0500

    git add explain modify array with filter

---
+++ tema19/index.js
@@ -0,0 +1,58 @@
+// filtrar un Array
+// Arreglos
+
+let persona1 = {
+  nombre: "Ernesto",
+  apellido: "Campos",
+  altura: 1.78
+};
+
+let persona2 = {
+  nombre: "Patricia",
+  apellido: "Villa",
+  altura: 1.7
+};
+
+let persona3 = {
+  nombre: "Elias",
+  apellido: "Campos",
+  altura: 1.73
+};
+
+let persona4 = {
+  nombre: "Carlos",
+  apellido: "Campos",
+  altura: 1.75
+};
+
+let persona5 = {
+  nombre: "Juan",
+  apellido: "Campos",
+  altura: 1.85
+};
+
+let persona6 = {
+  nombre: "Venus",
+  apellido: "Cardenas",
+  altura: 1.65
+};
+let grupoPersonas = [
+  persona1,
+  persona2,
+  persona3,
+  persona4,
+  persona5,
+  persona6
+];
+
+const esAlta = ({ altura }) => altura > 1.75;
+const esBaja = ({ altura }) => altura < 1.7;
+// filtrar a las personas que son mas altas que un medida
+let personasAltas = grupoPersonas.filter(esAlta);
+
+let personasBajas = grupoPersonas.filter(esBaja);
+
+// Otra forma de hacer este filtro es por medio de funciones anonimas
+// let personasAltas = grupoPersonas.filter(function(persona) {
+//   return persona.altura > 1.75;
+// });
