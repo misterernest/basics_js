2f3c3f83671982f1451ac41153a2fd9e9709440e
Ernesto Campos Villa <holasoy@misterernest.com>
2019-07-13 23:34:19 -0500

    add explain while

--- tema16/index.js
+++ tema16/index.js
@@ -1,14 +1,29 @@
-// ciclos do while
+// ciclos while
+var persona = {
+  nombre: "Ernesto",
+  apellido: "Campos",
+  edad: 37,
+  peso: 70
+};
+console.log(`Al inicio del año ${persona.nombre} pesaba ${persona.peso} kg`);
+const INCREMENTO_DE_PESO = 0.3;
+const DAY_OF_YEAR = 365;
+const aumentarDePeso = persona => (persona.peso += INCREMENTO_DE_PESO);
+const adelgazar = persona => (persona.peso -= INCREMENTO_DE_PESO);
 
-let contador = 0;
-
-const llueve = () => Math.random() < 0.3;
-
-do {
-  contador++;
-} while (!llueve());
-let palabraPlural = "vez";
-if (contador > 1) {
-  palabraPlural = "veces";
+const comeMucho = () => Math.random() < 0.3;
+const realizaDeporte = () => Math.random() < 0.4;
+// la persona debe bajar de peso 3kg
+const META = persona.peso - 3;
+let dias = 0;
+while (persona.peso > META) {
+  if (comeMucho()) {
+    aumentarDePeso(persona);
+  }
+  if (realizaDeporte()) {
+    adelgazar(persona);
+  }
+  dias += 1;
 }
-console.log(`Fui a ver si llovia ${contador} ${palabraPlural}`);
+
+console.log(`pasaron ${dias} días antes de que ${persona.nombre} adelgazara.`);
